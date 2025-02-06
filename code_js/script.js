6065c4e90f29ebdfde978f993d2ccc5cbf3e5abb
Alexis Evaristo <alex31.eva@icloud.com>
2025-02-06 16:56:06 +0100

    commit en utilisant lazygit

--- code_js/script.js
+++ code_js/script.js
@@ -2,10 +2,16 @@ let count = 0;
 
 function toggleLanguage() {
   if (count % 2 === 0) {
-    document.getElementById("language").innerHTML = "English";
+    document.getElementById("language").innerHTML = "🇫🇷Français";
     count += 1;
+
+    //on modifie des paragraphes et des entêtes:
+    document.getElementById("home0").innerHTML = "Home";
+    document.getElementById("home1").innerHTML = "Home";
   } else {
-    document.getElementById("language").innerHTML = "Français";
+    document.getElementById("language").innerHTML = "🇬🇧Anglais";
     count += 1;
+    document.getElementById("home0").innerHTML = "Acceuil";
+    document.getElementById("home1").innerHTML = "Acceuil";
   }
 }
